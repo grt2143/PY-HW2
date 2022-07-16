37fe6c0599bc5df1ab3795a3e289a64ae56a0292
Alex <alexchurilov@mail.ru>
2022-07-16 12:15:15 +0300

    Add files

---
+++ Funct.py
@@ -0,0 +1,21 @@
+def input_number_test_float(text):
+
+    int_test = True
+    is_minus = False
+    while int_test:
+        coord = input(f"(text)")
+        if coord[0] == "-":
+            is_minus = True
+            coord = coord.replace("-","")
+        if coord.isdigit():
+            coord = int(coord)
+            if is_minus:
+                coord *= -1
+            int_test = False
+        elif coord.isdecimal:
+            coord = float(coord)
+            if is_minus:
+                coord *= -1
+            int_test = False
+        else :
+            print("Введенное значение не является числом")
---
+++ Task_1.py
@@ -0,0 +1,17 @@
+# Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+
+# *Пример:*
+# 6782 -> 23
+# 0,56 -> 11
+
+from funct import input_number_test_float
+
+number = str(input_number_test_float("Введите число: "))
+
+res = 0
+
+for i in number:
+    if i.isdigit():
+        res = res + int(i)
+
+print(f"Сумма цифр в числе {number} равна {res}")
